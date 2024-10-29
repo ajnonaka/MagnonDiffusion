f510e571db11cebd09c792d68c1c954bc8e0e19c
prkkumar <prabhatams@gmail.com>
2024-10-29 12:37:23 -0700

    Cv, currents, and bfo input

--- Source/main.cpp
+++ Source/main.cpp
@@ -60,6 +60,15 @@ void main_main (c_MagnonDiffusion& rMagnonDiffusion)
     MultiFab T_old(ba, dm, Ncomp, Nghost);
     MultiFab T_new(ba, dm, Ncomp, Nghost);
 
+    //MultiFab to store currents. Only used for diagnostics
+    Array<MultiFab, AMREX_SPACEDIM> J_mu;
+    Array<MultiFab, AMREX_SPACEDIM> J_T;
+    for (int dir = 0; dir < AMREX_SPACEDIM; dir++)
+    {
+        J_mu[dir].define(ba, dm, Ncomp, 1);
+        J_T[dir].define(ba, dm, Ncomp, 1);
+    }
+
     //MultiFabs to parse robin BC coefficients for mu
     MultiFab robin_hi_a(ba, dm, Ncomp, Nghost);
     MultiFab robin_hi_b(ba, dm, Ncomp, Nghost);
@@ -80,9 +89,11 @@ void main_main (c_MagnonDiffusion& rMagnonDiffusion)
     MultiFab sigma(ba, dm, 1, 1); 
     MultiFab lambda(ba, dm, 1, 1); 
     MultiFab kappa(ba, dm, 1, 1); 
+    MultiFab Cv_mf(ba, dm, 1, 1); 
     sigma.setVal(0.); //initialize with zero. will be filled using parser
     lambda.setVal(0.);
     kappa.setVal(0.);
+    Cv_mf.setVal(2.6e6);
    
     MultiFab acoef_mf(ba, dm, 1, 1);
     MultiFab bcoef_mf(ba, dm, 1, 1);
@@ -101,11 +112,12 @@ void main_main (c_MagnonDiffusion& rMagnonDiffusion)
     initialize_lambda_using_parser(rMagnonDiffusion, geom, lambda); //read position dependent sigma, lambda, and kappa using parser 
     initialize_kappa_using_parser(rMagnonDiffusion, geom, kappa); //read position dependent sigma, lambda, and kappa using parser 
     initialize_jc_using_parser(rMagnonDiffusion, geom, Jc_mf); //read position dependent sigma, lambda, and kappa using parser 
+    initialize_Cv_using_parser(rMagnonDiffusion, geom, Cv_mf); //read position dependent sigma, lambda, and kappa using parser 
 
 #ifdef AMREX_USE_EB
-    MultiFab Plt(ba, dm, 8, 0,  MFInfo(), *rGprop.pEB->p_factory_union);
+    MultiFab Plt(ba, dm, 14, 0,  MFInfo(), *rGprop.pEB->p_factory_union);
 #else    
-    MultiFab Plt(ba, dm, 8, 0);
+    MultiFab Plt(ba, dm, 14, 0);
 #endif
 
     // Initialize mu_new here
@@ -127,15 +139,15 @@ void main_main (c_MagnonDiffusion& rMagnonDiffusion)
         int n = 0;
         const std::string& pltfile = amrex::Concatenate("plt",n,5);
 #ifdef AMREX_USE_EB
-        EB_WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f", "sigma", "Jc", "kappa"}, geom, time, n);
+            EB_WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f","sigma", "Jc", "kappa", "J_mux", "J_muy", "J_muz", "J_Tx", "J_Ty", "J_Tz"}, geom, time, n);
 #else    
-        WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f", "sigma", "Jc", "kappa"}, geom, time, n);
+            WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f", "sigma", "Jc", "kappa", "J_mux", "J_muy", "J_muz", "J_Tx"    , "J_Ty", "J_Tz"}, geom, time, n);
 #endif
     }
 
     fill_acoef(acoef_mf, sigma, lambda); //fill a coef acoef_mf(i,j,k) = 1 + sigma(i,j,k)*dt/lambda(i,j,k)^2
     fill_bcoef(bcoef_mf, sigma); //bcoef_mf(i,j,k) = dt*sigma(i,j,k)
-    fill_bcoef(T_bcoef_mf, kappa); //T_bcoef_mf(i,j,k) = dt*kappa(i,j,k)
+    fill_bcoef_T(T_bcoef_mf, kappa, Cv_mf); //T_bcoef_mf(i,j,k) = dt*kappa(i,j,k)/Cv(i,j,k)
     
     for (int n = 1; n <= nsteps; ++n)
     {
@@ -145,8 +157,13 @@ void main_main (c_MagnonDiffusion& rMagnonDiffusion)
         // new_mu = (I-dt)^{-1} * old_mu + dt
         // magnon diffusion case has updated alpha and beta coeffs
         // (a * alpha * I - b del*beta del ) mu = RHS
-        advance_T(T_old, T_new, mu_new, T_robin_hi_a, T_robin_hi_b, T_robin_hi_f, T_bcoef_mf, sigma, kappa, Jc_mf, rMagnonDiffusion, geom);
-        advance_mu(mu_old, mu_new, T_new, robin_hi_a, robin_hi_b, robin_hi_f, sigma, acoef_mf, bcoef_mf, rMagnonDiffusion, geom);
+        advance_T(T_old, T_new, mu_new, J_T, T_robin_hi_a, T_robin_hi_b, T_robin_hi_f, T_bcoef_mf, sigma, kappa, Jc_mf, Cv_mf, rMagnonDiffusion, geom);
+        advance_mu(mu_old, mu_new, T_new, J_mu, robin_hi_a, robin_hi_b, robin_hi_f, sigma, acoef_mf, bcoef_mf, rMagnonDiffusion, geom);
+        
+        //Now J_mu and J_T contain grad(mu) and grad(T) respectively. modify in place to get J_mu = -sigma*grad(mu) - sigma*S*grad(T)
+        //J_mu = J_m,diff + j_m,q: 2016 VanBees PRB 94, 174437 (2016)
+        //J_T = -kappa*grad(T)
+        compute_currents(J_mu, J_T, sigma, kappa); 
         time = time + dt;
 
         // Tell the I/O Processor to write out which step we're doing
@@ -158,12 +175,18 @@ void main_main (c_MagnonDiffusion& rMagnonDiffusion)
             // Write a plotfile of the current data (plot_int was defined in the inputs file)
             MultiFab::Copy(Plt, mu_new, 0, 0, 1, 0);
             MultiFab::Copy(Plt, T_new, 0, 1, 1, 0);
+            MultiFab::Copy(Plt, J_mu[0], 0, 8, 1, 0);
+            MultiFab::Copy(Plt, J_mu[1], 0, 9, 1, 0);
+            MultiFab::Copy(Plt, J_mu[2], 0, 10, 1, 0);
+            MultiFab::Copy(Plt, J_T[0], 0, 11, 1, 0);
+            MultiFab::Copy(Plt, J_T[1], 0, 12, 1, 0);
+            MultiFab::Copy(Plt, J_T[2], 0, 13, 1, 0);
 
             const std::string& pltfile = amrex::Concatenate("plt",n,5);
 #ifdef AMREX_USE_EB
-            EB_WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f","sigma", "Jc", "kappa"}, geom, time, n);
+            EB_WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f","sigma", "Jc", "kappa", "J_mux", "J_muy", "J_muz", "J_Tx", "J_Ty", "J_Tz"}, geom, time, n);
 #else    
-            WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f", "sigma", "Jc", "kappa"}, geom, time, n);
+            WriteSingleLevelPlotfile(pltfile, Plt, {"mu", "T", "robin_a", "robin_b", "robin_f", "sigma", "Jc", "kappa", "J_mux", "J_muy", "J_muz", "J_Tx"    , "J_Ty", "J_Tz"}, geom, time, n);
 #endif
         }
     }
